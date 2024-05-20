d61bfa27acf3cda77373b0920258442828a26ec2
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-05-20 14:57:05 +0300

    Added testing projects for GUI. Applied configuration

---
+++ gui/AB15_GUI.Regression/Bugs/UnitTest2.cs
@@ -0,0 +1,23 @@
+using System;
+using NUnit.Framework;
+using FlaUI.Core;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+
+namespace AB15_GUI.Regression.Bugs
+{
+    public class Tests_tmp2
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ gui/AB15_GUI.Tests/Models/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AB15_GUI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
