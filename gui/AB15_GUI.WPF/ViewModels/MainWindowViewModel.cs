234817f804067c00f10995a2a9c0d9b2f342f401
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-05-24 17:21:57 +0300

    Added empty ViewModel class

---
+++ gui/AB15_GUI.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AB15_GUI.WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        // TODO: add public properties for contained view models
+
+        public MainWindowViewModel()
+        {
+            // TODO: Instantiate view models
+
+
+            //// TODO: connect logger data in ViewModel
+            //LogMemoryRecordtTarget target = (LogMemoryRecordtTarget)LogManager.Configuration.FindTargetByName("memory");
+            //var log = string.Join("\r\n", target.Logs);
+        }
+    }
+}
