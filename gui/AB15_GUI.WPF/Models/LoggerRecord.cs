f9fcd4dd1ce0604a8871660b927845d3180a88bb
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-05-24 17:21:05 +0300

    Added model for log record for UI

---
+++ gui/AB15_GUI.WPF/Models/LoggerRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AB15_GUI.WPF.Models
+{
+    public class LoggerRecord
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public string? Level { get; set; }
+
+        public int? Index { get; set; }
+
+        public string? Message { get; set; }
+
+        // TODO: add info about class and line number?
+    }
+}
