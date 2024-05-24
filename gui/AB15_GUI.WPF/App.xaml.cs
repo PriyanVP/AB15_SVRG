ab539398b8977d37f268f44c2b8ad8d3feb113bb
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-05-24 17:23:32 +0300

    Added ViewModel binding. Initialized logger. Configured OnExit event

--- gui/AB15_GUI.WPF/App.xaml.cs
+++ gui/AB15_GUI.WPF/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿using System.Configuration;
+﻿using AB15_GUI.WPF.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using NLog;
+using NLog.Targets;
+using System;
+using System.Reflection;
+using NLog.Config;
+using System.Xml.Linq;
+using AB15_GUI.WPF.NLog;
 
 namespace AB15_GUI.WPF
 {
@@ -9,16 +17,36 @@ namespace AB15_GUI.WPF
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Getting Logger reference with custom configuration
+        /// </summary>
+        private static Logger Logger = LogManager.Setup()
+                                                 .SetupExtensions(ext => ext.RegisterLayoutRenderer<BuildConfigurationLayoutRenderer>("build-configuration"))
+                                                 .SetupExtensions(ext => ext.RegisterTarget<LogMemoryRecordtTarget>("MemoryRecord"))
+                                                 .GetCurrentClassLogger();
+
+        public App()
+        {
+            Logger.Info("Starting application.");
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new MainWindow();
-
-            // TODO: databinding to MainWindowView
-
+            Logger.Info("App startup event");
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainWindowViewModel()
+            };
             MainWindow.Show();
             
             base.OnStartup(e);
         }
-    }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Logger.Info("App exit event");
+            LogManager.Shutdown();
+            base.OnExit(e);
+        }
+    }
 }
