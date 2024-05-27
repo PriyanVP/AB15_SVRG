9aaa97ef7e47189de0872623357dbb1742d78f8a
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-05-27 10:13:25 +0300

    Updated CommandBase to RelayCommand

---
+++ gui/AB15_GUI.WPF/ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Windows.Input;
+
+namespace AB15_GUI.WPF.ViewModels.Commands
+{
+    /// <summary>
+    /// Relay command implementation
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        /// <summary>
+        /// Creates new Relay Command
+        /// </summary>
+        /// <param name="execute">function that will be executed</param>
+        /// <param name="canExecute">function that checks if execute can run. If not provided returns true</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return (canExecute == null) ? (true) : (canExecute(parameter));
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
