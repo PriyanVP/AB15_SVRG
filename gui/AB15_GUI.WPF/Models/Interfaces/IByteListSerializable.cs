716a8f3b48fb85a5e14d19d6beef3968fee08f65
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-06-11 16:26:54 +0300

    Added initial version of interfaces for communication service

---
+++ gui/AB15_GUI.WPF/Models/Interfaces/IByteListSerializable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace AB15_GUI.WPF.Models.Interfaces
+{
+    /// <summary>
+    /// Interface exposing methods for objects that hold payload data
+    /// </summary>
+    public interface IByteListSerializable
+    {
+        /// <summary>
+        /// Convert information stored in object to List of bytes
+        /// </summary>
+        /// <returns>object data in form of List of bytes</returns>
+        List<byte> Serialize();
+
+        /// <summary>
+        /// Convert List of bytes containing object information back to object fields
+        /// </summary>
+        /// <param name="rawData">List of bytes containing object information</param>
+        void Deserialize(List<byte> rawData);
+    }
+}
---
+++ gui/AB15_GUI.WPF/Models/Interfaces/ITransmitCommunicationPackage.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace AB15_GUI.WPF.Models.Interfaces
+{
+    /// <summary>
+    /// Interface for packages that are transmitted
+    /// </summary>
+    public interface ITransmitCommunicationPackage
+    {
+        /// <summary>
+        /// Message ID
+        /// </summary>
+        int MsgID { get; }
+
+        /// <summary>
+        /// ASIC ID
+        /// </summary>
+        int ASICID { get; }
+
+        /// <summary>
+        /// Command to send in package
+        /// </summary>
+        MCUCommand Cmd { get; set; }
+
+        /// <summary>
+        /// TODO: redo?
+        /// </summary>
+        /// <value></value>
+        bool IsPackageValid { get; }
+       
+        /// <summary>
+        /// Format data to send in byte list suitable for use in communication
+        /// </summary>
+        /// <returns>List with bytes to be send to MCU</returns>
+        List<byte> GetPackage();
+    }
+}
