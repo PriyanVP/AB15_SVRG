d3561ea242168f6f186d833ad5d3e421c5acd8ab
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-06-14 16:55:00 +0300

    Added initial interfaces implementation for communication classes

---
+++ gui/AB15_GUI.WPF/Services/Interfaces/ISerialWrapper.cs
@@ -0,0 +1,24 @@
+﻿using AB15_GUI.WPF.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace AB15_GUI.WPF.Services.Interfaces
+{
+    /// <summary>
+    /// Interface for serial communication wrapper
+    /// </summary>
+    public interface ISerialWrapper
+    {
+        /// <summary>
+        /// TODO: add information
+        /// </summary>
+        List<string> AvailableCOMPorts { get; }
+        bool IsCOMPortPresent { get; }
+        string? ManualComPortName { get; set; }
+
+        void Dispose();
+        bool ReconnectCOMPort();
+        bool RemoveWaitlistItem(int? msgID);
+        bool SerialWrite(ITransmitCommunicationPackage packageToSend, Action<IReceiveCommunicationPackage>? deleg = null, bool isContinuous = false);
+    }
+}
---
+++ gui/AB15_GUI.WPF/Services/Interfaces/IWaitlist.cs
@@ -0,0 +1,14 @@
+﻿using AB15_GUI.WPF.Models.Interfaces;
+using System;
+
+namespace AB15_GUI.WPF.Services.Interfaces
+{
+    public interface IWaitlist
+    {
+        (int msgID, bool isAddedSuccessfully) AddItemToWaitlist(Action<IReceiveCommunicationPackage> deleg, Type payloadType, bool isContinuous = false);
+        void ClearWaitlist();
+        Action<IReceiveCommunicationPackage>? GetDelegate(IReceiveCommunicationPackage receivedPackage);
+        Type? GetPayloadType(int msgID);
+        bool RemoveItemFromWaitlist(int? msgID);
+    }
+}
