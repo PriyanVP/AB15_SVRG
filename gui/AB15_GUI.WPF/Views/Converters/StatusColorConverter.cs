b5c6663af86a3b75bcb0da2761833623d8da092a
Nikita Tkachenko <nikita.tkachenko@ua.bosch.com>
2024-06-26 13:59:54 +0300

    add comments

--- gui/AB15_GUI.WPF/Views/Converters/StatusColorConverter.cs
+++ gui/AB15_GUI.WPF/Views/Converters/StatusColorConverter.cs
@@ -12,19 +12,20 @@ using AB15_GUI.WPF.ViewModels;
 namespace AB15_GUI.WPF.Views.Converters
 {
     /// <summary>
-    /// 
+    /// Convert enum status to color representation
     /// </summary>
     class StatusColorConverter : IValueConverter
     {
         /// <summary>
-        /// 
+        /// Based on input values conver enum value to color
         /// </summary>
         /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-
+            //getting status
             UIConnectionStatus status = (UIConnectionStatus)value;
 
+            // returning color based on status
             switch (status)
             {
                 case UIConnectionStatus.NotConnected:
