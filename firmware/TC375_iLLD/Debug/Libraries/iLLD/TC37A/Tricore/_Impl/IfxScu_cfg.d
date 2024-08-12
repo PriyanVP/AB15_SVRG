12aa6b99577de5f9f7526b0b9d2188e335da609f
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-08-12 15:24:26 +0300

    Stoped tracking files

--- firmware/TC375_iLLD/Debug/Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.d
+++
@@ -1,8 +0,0 @@
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.c
-../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.c :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.h
-../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.h :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	c:/Users/DUO7KV/Projects/ab15_sw/firmware/TC375_iLLD/Configurations/Ifx_Cfg.h
-c:/Users/DUO7KV/Projects/ab15_sw/firmware/TC375_iLLD/Configurations/Ifx_Cfg.h :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	c:/Users/DUO7KV/Projects/ab15_sw/firmware/TC375_iLLD/Libraries/Infra/Sfr/TC37A/_Reg/IfxScu_bf.h
-c:/Users/DUO7KV/Projects/ab15_sw/firmware/TC375_iLLD/Libraries/Infra/Sfr/TC37A/_Reg/IfxScu_bf.h :
