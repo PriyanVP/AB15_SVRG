ca86ec458c5f44a7b5b8157faa7cb2866c98c67e
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-08-22 17:52:08 +0300

    Refactored DeviceID test

--- firmware/AB15_MCU.Tests/Tests/test_DeviceID.py
+++ firmware/AB15_MCU.Tests/Tests/test_DeviceID.py
@@ -5,11 +5,9 @@
 import os
 import pytest
 import serial
-import time
-from fixtures.serial_fixtures import SerialWrapper
-
-# Change working directory to script directory TODO: investigate why it doesn't work as expected
-#os.chdir(os.path.dirname(__file__))
+from time import sleep
+from serial_fixtures import SerialWrapper
+import package_helper as pkg
 
 class TestDeviceID:
     '''DeviceId command tests'''
@@ -21,36 +19,42 @@ class TestDeviceID:
     # AB12 IC device ID
     DEVICE_ID_AB12 = 0xC4
 
-    # USB commands tested, from AB_protocol_description.xlsx, corresponds to CmdGetMcuVersion()
-    GET_MCU_VERSION = 0xAB0000120028BA
-    READ_DEV_ID = 0xAB0F010300D3BA
+    @classmethod
+    def setup_class(cls):
+        # COM port handling; connects to COM port with SW TC375 by default once before executing tests in class
+        cls.serial = SerialWrapper()
+        cls.serial.open_port()
+        print ('\nsetup_class()')
 
-    @pytest.fixture() 
-    # COM port handling; connects to COM14 by default before each test, 
-    # disconnects after test's completion
-    def serial(self):
-        # setup
-        serial = SerialWrapper()
-        serial.open_port()
-        yield serial
-        # teardown
-        serial.close_port()
+    @classmethod 
+    def teardown_class(cls):
+        # disconnects after test's completion
+        cls.close_port()
+        print ('\nteardown_class()')
+
 
     @pytest.mark.basic
     @pytest.mark.serial
-    def test_COMport(self, serial):
+    def test_COMport(self):
         '''group `basic` tests
         tests:
         - ShieldBuddy's serial connection to PC'''
 
+        # TODO: check if approach works
+
+        # Arrange
+
+        # Act
+
+        # Assert
         assert serial.com_port.is_open == True
 
         # Output to be captured if test passes
-        print("ShieldBuddy is connected sucesfully at port", serial.com_port.name)
+        print("ShieldBuddy is connected sucesfully at port", self.com_port.name)
 
     @pytest.mark.serial
     @pytest.mark.basic
-    def test_MCUVersion(self, serial):
+    def test_MCUVersion(self):
         '''group basic tests
         verifies ShieldBuddy's firmware version
         tests:
@@ -58,20 +62,26 @@ class TestDeviceID:
         - MCU command:
             * GET_MCU_BUILD_VERSION'''
 
-        user_cmd = self.GET_MCU_VERSION
-        serial.com_port.write(int(user_cmd).to_bytes(7, 'big'))
-        time.sleep(1)
-        result = serial.com_port.read(10)
+        # Arrange
+        packageToSend = pkg.TransmitPackage(0x00, 0x00, pkg.Command.GET_MCU_VERSION)
 
+        # Act
+        self.com_port.write(packageToSend.serialize())
+        sleep(0.1)
+        is_response_received = self.com_port.extract_packages()
+        result = self.packages.pop(0)
+
+        # Assert
+        assert is_response_received
         assert (result[5] == self.VERSION_MAJOR) and (result[6] == self.VERSION_MINOR) and (result[7] == self.VERSION_PATCH)
 
         # Output to be captured if test passes
-        print('Firmware version: ', hex(result[5]), hex(result[6]), hex(result[7])) # expected 0x0 0x2 0x0
+        print(f'Firmware version: {hex(result[5])}.{hex(result[6])}.{hex(result[7])}') # expected 0x0 0x2 0x0
 
     @pytest.mark.serial
     @pytest.mark.basic
     @pytest.mark.skip(reason="Skipped due no HW connected (AB12/15 ASIC board)")
-    def test_DeviceID(self, serial):
+    def test_DeviceID(self):
         #Add documentation comments for functions of test files
         '''group basic tests
         verifies AB15(12) DeviceID
@@ -79,11 +89,16 @@ class TestDeviceID:
         - MCU command:
             * READ_DEV_ID'''
 
-        user_cmd = self.READ_DEV_ID
-        serial.com_port.write(int(user_cmd).to_bytes(7, 'big'))
-        time.sleep(1)
-        result = serial.com_port.read(8)
+        # Arrange
+        packageToSend = pkg.TransmitPackage(0x0F, 0x01, pkg.Command.READ_DEV_ID)
 
+        # Act
+        self.com_port.write(packageToSend.serialize())
+
+        sleep(0.1)
+        result = self.com_port.read(8)
+
+        # Assert
         assert (result[5] == DEVICE_ID_AB12)
 
         # Output to be captured if test passes
