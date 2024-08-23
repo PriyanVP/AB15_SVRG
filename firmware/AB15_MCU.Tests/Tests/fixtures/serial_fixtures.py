942c2ce3afdf2748353142b778aa072056c452d0
Vadym Khudobets <vadym.khudobets@ua.bosch.com>
2024-08-23 15:45:53 +0300

    bugs fixed: automatic COM connection, class access

--- firmware/AB15_MCU.Tests/Tests/fixtures/serial_fixtures.py
+++ firmware/AB15_MCU.Tests/Tests/fixtures/serial_fixtures.py
@@ -12,7 +12,7 @@ class SerialWrapper():
     VENDOR_ID = 0x058B  # Infineon DAS JDS - vendor ID
     PRODUCT_ID = 0x0043 # Infineon DAS JDS - product_ID
 
-    def __init__(self, port_name:str=None):
+    def __init__(self, port_name:str=""):
         # Protocol characteristics
         self.com_port = serial.Serial()
         self.com_port.baudrate = 115200
@@ -22,7 +22,7 @@ class SerialWrapper():
         self.com_port.timeout = 1 # in seconds
 
         # COM port selection
-        if (port_name is not None):
+        if (port_name != ""):
             self.com_port.port = port_name
         else:
             try:
@@ -50,7 +50,10 @@ class SerialWrapper():
         # Find COM ports with SB TC375 connected to it
         sb_ports_list = []
         for port in sorted(ports):
-            if ((port.vid == VENDOR_ID) and (port.pid == PORT_ID)):
+            print("port.description, port.vid, port.pid")
+            print(port.description, port.vid, port.pid)
+            if ((port.vid == self.VENDOR_ID) and (port.pid == self.PRODUCT_ID)):
+                print("Shieldbuddy found")
                 sb_ports_list.append(port.name)
         return sb_ports_list
 
@@ -58,13 +61,13 @@ class SerialWrapper():
         '''Extract MCU responses from input com port buffer and store them to attribute
             Warning: won't report if invalid data was received - will just skip it
             Returns true if at least one package was extracted'''
-        is_package_found = false
+        is_package_found = False
         tmp_package = []
 
         while (self.com_port.in_waiting > pkg_const.MIN_PACKAGE_LENGTH):
             # Read first byte
             tmp = self.com_port.read(pkg_const.START_BYTE_LENGTH) 
-            tmp_package.append(*[byte for byte in tmp]) # append bytes from buffer as individual int values
+            tmp_package.extend([byte for byte in tmp]) # append bytes from buffer as individual int values
 
             # If first byte is incorrect, remove it and try next byte
             if (tmp_package[0] != pkg_const.START_BYTE_VALUE):
@@ -73,17 +76,17 @@ class SerialWrapper():
 
             # Read bytes till payload length
             tmp = self.com_port.read(pkg_const.MSG_ID_LENGTH + pkg_const.ASIC_ID_LENGTH + pkg_const.CMD_STATUS_LENGTH + pkg_const.PAYLOAD_LENGTH_LENGTH) 
-            tmp_package.append(*[byte for byte in tmp]) # append bytes from buffer as individual int values
+            tmp_package.extend([byte for byte in tmp]) # append bytes from buffer as individual int values
 
             # Check if enough items in buffer
-            remaining_package_length = tmp_package[pkg_const.PAYLOAD_POSITION] + pkg_const.CRC_LENGTH + pkg_const.END_BYTE_LENGTH
+            remaining_package_length = tmp_package[pkg_const.PAYLOAD_LENGTH_POSITION] + pkg_const.CRC_LENGTH + pkg_const.END_BYTE_LENGTH
             if (self.com_port.in_waiting < remaining_package_length):
                 tmp_package.clear()
                 continue
 
             # Read remaining bytes in package
             tmp = self.com_port.read(remaining_package_length) 
-            tmp_package.append(*[byte for byte in tmp]) # append bytes from buffer as individual int values
+            tmp_package.extend([byte for byte in tmp]) # append bytes from buffer as individual int values
 
             # Report that package was found
             is_package_found = True
@@ -98,5 +101,5 @@ class SerialWrapper():
         return is_package_found
 
 if __name__ == "__main__":
-    serial_tst = SerialWrapper('COM14')
+    serial_tst = SerialWrapper()
     # boards = serial_tst.find_connected_boards()
