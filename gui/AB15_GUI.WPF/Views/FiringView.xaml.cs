a1b21767e7d3f9b5bacbe8158a0824abc6d8f299
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-11-05 15:38:08 +0200

    GUI: removed test code; added command for drop down list

--- gui/AB15_GUI.WPF/Views/FiringView.xaml.cs
+++ gui/AB15_GUI.WPF/Views/FiringView.xaml.cs
@@ -1,19 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AB15_GUI.WPF.Views
 {
@@ -22,74 +8,9 @@ namespace AB15_GUI.WPF.Views
     /// </summary>
     public partial class Firing : Page
     {
-        public ObservableCollection<TestInput> ConfigChannelsTable { get; set; }
-        public ObservableCollection<TestOutput> FiringResultTable { get; set; }
-
         public Firing()
         {
-
-            this.ConfigChannelsTable = new ObservableCollection<TestInput>();
-            this.FiringResultTable = new ObservableCollection<TestOutput>();
             InitializeComponent();
-
-            this.DataContext = this;
-
-            this.ConfigChannelsTable.Add(new TestInput() { ASICID = 1, ChannelID = 1, IndexMode = 1 });
-            this.ConfigChannelsTable.Add(new TestInput() { ASICID = 1, ChannelID = 2, IndexMode = 2 });
-            this.ConfigChannelsTable.Add(new TestInput() { ASICID = 1, ChannelID = 3, IndexMode = 3 });
-            this.ConfigChannelsTable.Add(new TestInput() { ASICID = 1, ChannelID = 4, IndexMode = 4 });
-
-            this.FiringResultTable.Add(new TestOutput() { ASICID = 1, ChannelID = 1, ToFire = false, WasFired = true });
-            this.FiringResultTable.Add(new TestOutput() { ASICID = 1, ChannelID = 2, ToFire = false, WasFired = true });
-            this.FiringResultTable.Add(new TestOutput() { ASICID = 1, ChannelID = 3, ToFire = false, WasFired = true });
-            this.FiringResultTable.Add(new TestOutput() { ASICID = 1, ChannelID = 4, ToFire = false, WasFired = true });
-
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
-    public class TestInput
-    {
-        public int ASICID { get; set; }
-        public int ChannelID { get; set; }
-        public string IdentifierName { 
-            get; 
-            set; }
-        public int _indexMode;
-        public int IndexMode 
-        { 
-            get 
-            { 
-                return _indexMode; 
-            }
-            set 
-            { 
-                _indexMode = value;
-            }
-        }
-
-        public TestInput()
-        {
-            IdentifierName = "None";
-        }
-    }
-
-    public class TestOutput
-    {
-        public int ASICID { get; set; }
-        public int ChannelID { get; set; }
-        public string IdentifierName { get; set; }
-
-        public bool ToFire { get; set; }
-        public bool WasFired { get; set; }
-        public TestOutput()
-        {
-            IdentifierName = "None";
-        }
-    }
-
 }
