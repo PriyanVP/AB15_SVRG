23b1dbab12c870132e336091a20b4a22148f2b49
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-11-05 10:42:43 +0200

    GUI: reordered properties in IRegister for easier mapping to implementation classes

--- gui/AB15_GUI.WPF/Models/Interfaces/IRegister.cs
+++ gui/AB15_GUI.WPF/Models/Interfaces/IRegister.cs
@@ -8,34 +8,34 @@ namespace AB15_GUI.WPF.Models.Interfaces
     public interface IRegister
     {
         /// <summary>
-        /// Access level of this register
+        /// Reset value for register
         /// </summary>
-        string Access { get; }
+        ushort ResetValue { get; }
+
+        /// <summary>
+        /// Name of the register
+        /// </summary>
+        string? Name { get; }
 
         /// <summary>
         /// Absolute address of the register
         /// </summary>
-        ushort Address { get; }
+        static ushort Address { get; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        string? Description { get; }
+
+        /// <summary>
+        /// Access level of this register
+        /// </summary>
+        string? Access { get; }
 
         /// <summary>
         /// Data property. Constructs register value from fields on get. 
         /// Deconstruct register value by fields on set
         /// </summary>
         ushort Data { get; set; }
-
-        /// <summary>
-        /// Description
-        /// </summary>
-        string Description { get; }
-
-        /// <summary>
-        /// Name of the register
-        /// </summary>
-        string Name { get; }
-
-        /// <summary>
-        /// Reset value for register
-        /// </summary>
-        ushort ResetValue { get; }
     }
 }
