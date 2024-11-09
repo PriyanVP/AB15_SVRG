9207789e667682949712b60112e2ade45236a234
Vadym Khudobets <vadym.khudobets@ua.bosch.com>
2024-11-09 14:17:28 +0100

    FLMShortDiag() first iteration finished, FLMSquibDetErrDiag() updated

--- firmware/mcu_esw/common/flm_diagnostics.c
+++ firmware/mcu_esw/common/flm_diagnostics.c
@@ -7,10 +7,16 @@
 /*-----------------------------------------------------Includes------------------------------------------------------*/
 /*********************************************************************************************************************/
 #include "Ifx_Types.h"
+#include "common/tap_addrMap_ExportedMemMap_memoryMap.h"
+#include "common/spi_data_types.h"
+#include "common/flm_diagnostics.h"
 
 /*********************************************************************************************************************/
 /*------------------------------------------------------Macros-------------------------------------------------------*/
 /*********************************************************************************************************************/
+
+#define FLM_DIAG_READ_SHORT_REGS_COUNT      (5)     /** \brief Number of registers with results of IGL/IGH short and 
+                                                    leakage to battery or ground detection */
 
 /*********************************************************************************************************************/
 /*------------------------------------------------Function Prototypes------------------------------------------------*/
@@ -52,6 +58,10 @@ void SetFLMDiagMode();
  */
 void StartFLMDiag();
 
+// TODO: implement
+/** \brief get FLM diagnostic execution status from ASIC (ongoing/evaluated)
+ */
+flm_cycDiagExecStatusEnum FLMReadDiagExecStatus(void);
 
 
 /** \brief
@@ -66,7 +76,7 @@ bool CheckBatVoltage();
 
 static FLMCycDiagFaults g_FLMCycDiagFaultsValues;
 static flm_cycDiagExecStatusEnum g_FLMCycDiagExecStatus;
-static FLMCycDiagResults g_FLMCycDiagResultsValues;
+static FLMCycDiagResults g_flmCycDiagResultsValues;
 //bool static g_FLMDiagActive = 0; // similar should be available at top level to see if MCU is busy with FLM diag
 //bool static g_FLMDiagReady = 0;
 
@@ -83,12 +93,35 @@ void InitFLMDiag()
     // 'Squibs on all channels' for this implementation 
 }
 
-// read FLM_Read_Short_ch4_1 through FLM_Read_Short_ch20_17, store in FLMShortDiagResults
+// Diagnostic is running automatically by default, just read results
 void FLMShortDiag()
 {
-    // TODO: research for QSPIReadSequenceNormal()
+    SPIReceiveDataNormal data[FLM_DIAG_READ_SHORT_REGS_COUNT] = {0};
+    uint16 length = FLM_DIAG_READ_SHORT_REGS_COUNT;
+    boolean isSuccessfulFlag = FALSE;
+    uint16 flmDiagShortsRegsAddresses = {FLM_FLM_READ_SHORT_CH4_1, FLM_FLM_READ_SHORT_CH8_5, 
+                                         FLM_FLM_READ_SHORT_CH12_9, FLM_FLM_READ_SHORT_CH16_13, 
+                                         FLM_FLM_READ_SHORT_CH20_17};
     
+    // read FLM_Read_Short_ch4_1 through FLM_Read_Short_ch20_17, store in FLMShortDiagResults
+    SetFLMDiagExecStatus(FLM_DIAG_EXEC_STATUS_ONGOING);
+
+    // Read related registers from ASIC
+    isSuccessfulFlag = QSPIReadSequenceNormal(SPI1_CS1MASTER, flmDiagShortsRegsAddresses, &data[0].dw, &length);
+    
+    // Store results //TODO: check order of data
+    g_flmCycDiagResultsValues.flmShortDiagResults.FLM_Read_Short_ch4_1 = data[0].bf.output_data;
+    g_flmCycDiagResultsValues.flmShortDiagResults.FLM_Read_Short_ch8_5 = data[1].bf.output_data;
+    g_flmCycDiagResultsValues.flmShortDiagResults.FLM_Read_Short_ch12_9 = data[2].bf.output_data;
+    g_flmCycDiagResultsValues.flmShortDiagResults.FLM_Read_Short_ch16_13 = data[3].bf.output_data;
+    g_flmCycDiagResultsValues.flmShortDiagResults.FLM_Read_Short_ch20_17 = data[4].bf.output_data;
+
+    // results are stored, get back
+    SetFLMDiagExecStatus(FLM_DIAG_EXEC_STATUS_FINISHED);
+    
+    // TODO: quick result analisys to check for faults
     // if any active, FLMCycDiagFaultsValues.FLM_SC2G_SC2B_fault = TRUE
+    return;
 }
 
 void FLMVHxDiag()
@@ -133,7 +166,21 @@ void FLMSquibDetErrDiag() //
 {
     static FLMSquibDetErrDiagResults FLMSquibDetErrDiagResultsValues;
 
-    //
+        if (GetFLMDiagExecStatus() != FLM_DIAG_EXEC_STATUS_EVALUATED)
+    {
+        g_flmDiagExecStatus = FLM_DIAG_EXEC_STATUS_ONGOING;
+        // TODO: start diagnostic and get out
+        // code
+        return;
+    }
+    else
+    {
+        // TODO: diagnostic was performed, store results
+        // code
+        SetFLMDiagExecStatus(FLM_DIAG_EXEC_STATUS_FINISHED);
+    }
+
+    return;
     
 }
 
@@ -141,7 +188,21 @@ void FLMLoopResDiag() //
 {
     static FLMLoopResDiagResults FLMLoopResDiagResultsValues;
 
-    //
+    if (GetFLMDiagExecStatus() != FLM_DIAG_EXEC_STATUS_EVALUATED)
+    {
+        g_flmDiagExecStatus = FLM_DIAG_EXEC_STATUS_ONGOING;
+        // TODO: start diagnostic and get out
+        // code
+        return;
+    }
+    else
+    {
+        // TODO: diagnostic was performed, store results
+        // code
+        SetFLMDiagExecStatus(FLM_DIAG_EXEC_STATUS_FINISHED);
+    }
+
+    return;
     
 }
 
@@ -166,6 +227,14 @@ void StartFLMDiag(void)
 
     // Write
     QSPIWriteNormal(SPI1_CS1MASTER, FLM_FLM_DIAG_START, tmpFLMDiagStartfRegister.as_uint16);
+}
+
+flm_cycDiagExecStatusEnum FLMReadDiagExecStatus(void)
+{
+    // TODO
+    // Read flm_diag_active and FLM_diag_ready from ASIC
+    // (flm_diag_active ==1 && FLM_diag_ready == 0) -> SPI triggered diag is running)
+    // (flm_diag_active ==0 && flm_diag_ready ==1) -> SPI triggered diag is evaluated, read results)
 }
 
 void SetFLMDiagExecStatus(flm_cycDiagExecStatusEnum FLMCycDiagExecStatus)
