de400eb166781f1f88201642bbbaa398ad60a4fb
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-12-02 11:15:52 +0200

    GUI: potential fix for multithreading issue in logs display

--- gui/AB15_GUI.WPF/Models/ThreadSafeObservableList.cs
+++ gui/AB15_GUI.WPF/Models/ThreadSafeObservableList.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Collections;
-using AB15_GUI.WPF.Models;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.Specialized;
