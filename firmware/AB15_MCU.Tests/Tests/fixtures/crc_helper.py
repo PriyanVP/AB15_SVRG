3cc963ea3f7c0891d03f85cf15a4fe62e02745af
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2024-12-16 13:35:58 +0200

    GUI, MCU Tst: added CRC3 calculation functions; added unit tests

--- firmware/AB15_MCU.Tests/Tests/fixtures/crc_helper.py
+++ firmware/AB15_MCU.Tests/Tests/fixtures/crc_helper.py
@@ -2,6 +2,41 @@ from typeguard import typechecked
 from typing import List
 
 @typechecked
+def crc3(buffer: int, start_bit_position: int, end_bit_position: int) -> int:
+    '''CRC8 algorithm
+    Polynom: x3+x1+1, Initial value: 0x7'''
+
+    NUM_TRAILING_ZEROS = 3
+    input_data_msb_index = end_bit_position - start_bit_position + NUM_TRAILING_ZEROS # index of first MSB relevant for CRC
+
+    # Get variable with only relevant data for CRC calculation (starting from index 0)
+    # Clear MSB higher than end_bit_position -> clear LSB lower than start_bit_position -> shift relevant for CRC bits to 0 index -> add trailing 0
+    extended_buffer = (((buffer << (31 - end_bit_position)) & 0xFFFF_FFFF) >> (31 - end_bit_position + start_bit_position)) << NUM_TRAILING_ZEROS
+
+    # Store binary start value "111" into CRC register according to spec
+    bit0 = 0x1
+    bit1 = 0x1
+    bit2 = 0x1
+
+    # calculate CRC as in specification
+    # CRC polynomial: x^3 + x + 1 --> 1*x^3 + 0* x^2 + 1*x^1 + 1*x^0 --> poly = 1011
+    # - process data from MSB to LSB
+    # - XOR bits according to polynomial
+    for i in range(input_data_msb_index, -1, -1):
+        # Process bits
+        tmp_bit0 = bit2 ^ ((extended_buffer >> i) & 0x01)
+        tmp_bit1 = bit0 ^ bit2
+        tmp_bit2 = bit1
+
+        # Update with new values
+        bit0 = tmp_bit0
+        bit1 = tmp_bit1
+        bit2 = tmp_bit2
+
+    # CRC result
+    return ((bit2 << 2) | (bit1 << 1) | bit0)
+
+@typechecked
 def crc8(data_array: list, start_idx: int, length: int) -> int:
     '''CRC8 algorithm
     Polynom: x8+x2+x1+1, Initial value: 0x00'''
@@ -40,6 +75,19 @@ def crc16(data_array: List[int], start_idx: int, length: int) -> int:
 
 # Self test + usage example
 if __name__ == "__main__":
+    # CRC3 tests
+    frame = 0x0040_0010
+    start_bit_position = 5
+    end_bit_position = 31
+    crc = crc3(frame, start_bit_position, end_bit_position)
+    print(f"CRC3 Result 1: Actual 0x{crc:01X}, Expected 0x4")
+
+    frame = 0x3000_0C46
+    start_bit_position = 3
+    end_bit_position = 26
+    crc = crc3(frame, start_bit_position, end_bit_position)
+    print(f"CRC3 Result 2: Actual 0x{crc:01X}, Expected 0x6")
+
     # CRC8 tests
     data_array  = [0xAB, 0x0F, 0x01, 0x03, 0x00, 0xD3, 0xBA] # (CRC should be: 0xD3)
     start_idx = 1
