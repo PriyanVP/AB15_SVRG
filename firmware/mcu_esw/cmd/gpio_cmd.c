0535f43903739176fb7c990c087a36946f325dca
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2025-01-14 13:36:27 +0200

    MCU: removed/commented out unused functionalities

--- firmware/mcu_esw/cmd/gpio_cmd.c
+++ firmware/mcu_esw/cmd/gpio_cmd.c
@@ -27,106 +27,106 @@
 /*-------------------------------------------------Global variables--------------------------------------------------*/
 /*********************************************************************************************************************/
 
-static boolean g_GPIOPollingConfigured = FALSE;                     /** \brief Continuous read was configured        */
+// static boolean g_GPIOPollingConfigured = FALSE;                     /** \brief Continuous read was configured        */
 
-static USBReceiveData g_GPIOReadPackage;                            /** \brief Package to perform gpio read          */
+// static USBReceiveData g_GPIOReadPackage;                            /** \brief Package to perform gpio read          */
 
 /*********************************************************************************************************************/
 /*---------------------------------------------Function Implementations----------------------------------------------*/
 /*********************************************************************************************************************/
-void CmdStartGPIOReading(USBReceiveData const * const commandPackage)
-{
-    // Create empty package
-    USBTransmitData packageToSend;
-    packageToSend.device_id = commandPackage->device_id;
-    packageToSend.msg_id = SetResponseBit(commandPackage->msg_id);
+// void CmdStartGPIOReading(USBReceiveData const * const commandPackage)
+// {
+//     // Create empty package
+//     USBTransmitData packageToSend;
+//     packageToSend.device_id = commandPackage->device_id;
+//     packageToSend.msg_id = SetResponseBit(commandPackage->msg_id);
 
-    // Set main fields to default values
-    g_GPIOReadPackage.command = _USB_CMD_MIN;
-    g_GPIOReadPackage.msg_id = 0;
-    g_GPIOReadPackage.dataLength = 0;
+//     // Set main fields to default values
+//     g_GPIOReadPackage.command = _USB_CMD_MIN;
+//     g_GPIOReadPackage.msg_id = 0;
+//     g_GPIOReadPackage.dataLength = 0;
 
-    // Check if possible to start
-    if (GetStateGPIOInterrupt() == FALSE)
-    {
-        // Configure continuous read interrupt periodicity
-        if (commandPackage->dataLength >= 4)
-        {
-            uint16 continuousReadPeriodicity = ConstructWordFromBytes(commandPackage->data[1], commandPackage->data[0]);
-            ConfigureGPIOPeriodicity(continuousReadPeriodicity); // TODO: can be calculated on MCU side
+//     // Check if possible to start
+//     if (GetStateGPIOInterrupt() == FALSE)
+//     {
+//         // Configure continuous read interrupt periodicity
+//         if (commandPackage->dataLength >= 4)
+//         {
+//             uint16 continuousReadPeriodicity = ConstructWordFromBytes(commandPackage->data[1], commandPackage->data[0]);
+//             ConfigureTimerPeriodicity(GPIO_TIMER, continuousReadPeriodicity); // TODO: can be calculated on MCU side
 
-            // Store configuration to command
-            g_GPIOReadPackage.command = commandPackage->command;
-            g_GPIOReadPackage.dataLength = commandPackage->dataLength - 2;
-            for (uint8 i = 0; i < g_GPIOReadPackage.dataLength; i++)
-            {
-                g_GPIOReadPackage.data[i] = commandPackage->data[i + 2]; // 2 used because first 2 bytes used for periodicity configuration
-            }
+//             // Store configuration to command
+//             g_GPIOReadPackage.command = commandPackage->command;
+//             g_GPIOReadPackage.dataLength = commandPackage->dataLength - 2;
+//             for (uint8 i = 0; i < g_GPIOReadPackage.dataLength; i++)
+//             {
+//                 g_GPIOReadPackage.data[i] = commandPackage->data[i + 2]; // 2 used because first 2 bytes used for periodicity configuration
+//             }
 
-            // Set flag indicating finishing configuration of feature
-            g_GPIOPollingConfigured = TRUE;
+//             // Set flag indicating finishing configuration of feature
+//             g_GPIOPollingConfigured = TRUE;
 
-            // Report success
-            packageToSend.status = USB_STATUS_ACK;
-            packageToSend.dataLength = 0;
+//             // Report success
+//             packageToSend.status = USB_STATUS_ACK;
+//             packageToSend.dataLength = 0;
 
-            // Report feature state
-            SetGPIOState(FEATURE_CONFIGURED);
+//             // Report feature state
+//             SetGPIOState(FEATURE_CONFIGURED);
 
-            // Enable continuous read irq generation
-            EnableGPIOInterrupt();
+//             // Enable continuous read irq generation
+//             EnableTimerInterrupt(GPIO_TIMER);
 
-            // Set msg_id for responses
-            g_GPIOReadPackage.msg_id = commandPackage->msg_id;
+//             // Set msg_id for responses
+//             g_GPIOReadPackage.msg_id = commandPackage->msg_id;
 
-            // Report feature state
-            SetGPIOState(FEATURE_RUNNING);
-        }
-        else
-        {
-            // Clear flag indicating finishing configuration of feature
-            g_GPIOPollingConfigured = FALSE;
+//             // Report feature state
+//             SetGPIOState(FEATURE_RUNNING);
+//         }
+//         else
+//         {
+//             // Clear flag indicating finishing configuration of feature
+//             g_GPIOPollingConfigured = FALSE;
 
-            // Report error
-            packageToSend.status = USB_STATUS_ERROR;
-            packageToSend.dataLength = 0;
+//             // Report error
+//             packageToSend.status = USB_STATUS_ERROR;
+//             packageToSend.dataLength = 0;
 
-            // Report feature state
-            SetGPIOState(FEATURE_DISABLED);
-        }
+//             // Report feature state
+//             SetGPIOState(FEATURE_DISABLED);
+//         }
 
-        // Send response back to MCU
-        SendUSBPackage(&packageToSend);
-    }
-}
+//         // Send response back to MCU
+//         SendUSBPackage(&packageToSend);
+//     }
+// }
 
-void CmdStopGPIOReading(USBReceiveData const * const commandPackage)
-{
-    // Create empty package
-    USBTransmitData packageToSend;
+// void CmdStopGPIOReading(USBReceiveData const * const commandPackage)
+// {
+//     // Create empty package
+//     USBTransmitData packageToSend;
 
-    // Clear flag indicating finishing configuration of feature
-    g_GPIOPollingConfigured = FALSE;
+//     // Clear flag indicating finishing configuration of feature
+//     g_GPIOPollingConfigured = FALSE;
 
-    // Disable continuous read irq generation
-    DisableGPIOInterrupt();
+//     // Disable continuous read irq generation
+//     DisableTimerInterrupt(GPIO_TIMER);
 
-    // Send acknowlege back to MCU
-    packageToSend.device_id = commandPackage->device_id;
-    packageToSend.msg_id = SetResponseBit(commandPackage->msg_id);
-    packageToSend.status = USB_STATUS_ACK;
-    packageToSend.dataLength = 0;
-    SendUSBPackage(&packageToSend);
+//     // Send acknowlege back to MCU
+//     packageToSend.device_id = commandPackage->device_id;
+//     packageToSend.msg_id = SetResponseBit(commandPackage->msg_id);
+//     packageToSend.status = USB_STATUS_ACK;
+//     packageToSend.dataLength = 0;
+//     SendUSBPackage(&packageToSend);
 
-    // Report feature state
-    SetGPIOState(FEATURE_DISABLED);
-}
+//     // Report feature state
+//     SetGPIOState(FEATURE_DISABLED);
+// }
 
-void IntCmdGPIORead(void)
-{
-    // Stop executing if not configured
-    if (g_GPIOPollingConfigured == FALSE) return;
+// void IntCmdGPIORead(void)
+// {
+//     // Stop executing if not configured
+//     if (g_GPIOPollingConfigured == FALSE) return;
 
-    // Call function to read sequence of registers (addresses to read set during feature configuration)
-    // CmdExecuteReadSequence(&g_GPIOReadPackage); TODO: uncomment
-}
+//     // Call function to read sequence of registers (addresses to read set during feature configuration)
+//     // CmdExecuteReadSequence(&g_GPIOReadPackage); TODO: uncomment
+// }
--- firmware/mcu_esw/cmd/gpio_cmd.h
+++ firmware/mcu_esw/cmd/gpio_cmd.h
@@ -20,29 +20,29 @@
 /*------------------------------------------------Function Prototypes------------------------------------------------*/
 /*********************************************************************************************************************/
 
-/** \brief Handling USB_CMD_START_GPIO_READING command from PC
- * Populates read sequence for GPIO feature. Configure reading periodicity
- * Msg_id of this command will be used in responses of read iterations
- *
- * \param commandPackage package with command
- * \return Nothing
- */
-void CmdStartGPIOReading(USBReceiveData const * const commandPackage);
+// /** \brief Handling USB_CMD_START_GPIO_READING command from PC
+//  * Populates read sequence for GPIO feature. Configure reading periodicity
+//  * Msg_id of this command will be used in responses of read iterations
+//  *
+//  * \param commandPackage package with command
+//  * \return Nothing
+//  */
+// void CmdStartGPIOReading(USBReceiveData const * const commandPackage);
 
-/** \brief Handling USB_CMD_STOP_GPIO_READING command from PC
- * Stops GPIO reading if it was configured. Disables GPIO read interrupts
- *
- * \param commandPackage package with command
- * \return Nothing
- */
-void CmdStopGPIOReading(USBReceiveData const * const commandPackage);
+// /** \brief Handling USB_CMD_STOP_GPIO_READING command from PC
+//  * Stops GPIO reading if it was configured. Disables GPIO read interrupts
+//  *
+//  * \param commandPackage package with command
+//  * \return Nothing
+//  */
+// void CmdStopGPIOReading(USBReceiveData const * const commandPackage);
 
-/** \brief Handling INT_CMD_GPIO_READ command
- * Performs GPIO read iteration (single read of set of registers) if it was configured
- * Msg_id of response is set by start reading.
- *
- * \return Nothing
- */
-void IntCmdGPIORead(void);
+// /** \brief Handling INT_CMD_GPIO_READ command
+//  * Performs GPIO read iteration (single read of set of registers) if it was configured
+//  * Msg_id of response is set by start reading.
+//  *
+//  * \return Nothing
+//  */
+// void IntCmdGPIORead(void);
 
 #endif /* GPIO_CMD_H_ */
