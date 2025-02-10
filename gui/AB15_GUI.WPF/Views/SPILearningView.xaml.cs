ee1729d2186255f3801709bd6a8b6ab6ac6cd07b
Nikita Tkachenko <nikita.tkachenko@ua.bosch.com>
2025-02-11 00:03:57 +0200

    Add new validation

--- gui/AB15_GUI.WPF/Views/SPILearningView.xaml.cs
+++ gui/AB15_GUI.WPF/Views/SPILearningView.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace AB15_GUI.WPF.Views
 {
@@ -15,6 +19,9 @@ namespace AB15_GUI.WPF.Views
             InitializeComponent();
 
             ReadingCheckBox.IsChecked = true;
+
+            AddressTextBox.Text = "0";
+            DataTextBox.Text = "0";
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
@@ -57,6 +64,143 @@ namespace AB15_GUI.WPF.Views
             ReadWriteChooseField.Visibility = Visibility.Visible;
             RawFormatTextBox.Visibility = Visibility.Hidden;
             RawHexText.Visibility = Visibility.Hidden;
+        }
+        private async void ShowPopupMessage(TextBox textBox, string message)
+        {
+            ToolTip toolTip = new ToolTip
+            {
+                Content = message,
+                PlacementTarget = textBox,
+                Placement = System.Windows.Controls.Primitives.PlacementMode.Right,
+                IsOpen = true,
+                Style = (Style)FindResource("ToolTipError")
+            };
+
+            textBox.ToolTip = toolTip;
+            await Task.Delay(1000); // Show message for 1 second
+            toolTip.IsOpen = false;
+        }
+
+        private void HexAddressTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            string input = e.Text.ToUpper();
+
+            if (!Regex.IsMatch(input, "[0-9A-F]"))
+            {
+                ShowPopupMessage(textBox, "Invalid character! Use 0-9, A-F");
+                e.Handled = true;
+                return;
+            }
+
+            // Predict the new text if input is accepted
+            string newText = textBox.Text.Insert(textBox.SelectionStart, input);
+
+            if (newText.Length > 3)
+            {
+                ShowPopupMessage(textBox, "Invalid length! Max 3 symbols!");
+                e.Handled = true;
+                return;
+            }
+
+
+            // Ensure new text does not exceed MAXVALUE
+            if (TryParseHex(newText, out uint parsedValue) && parsedValue <= 0x3f0)
+            {
+                textBox.Text = newText;
+                textBox.SelectionStart = textBox.Text.Length;
+            }
+            else if (parsedValue > 0x3f0)
+            {
+                ShowPopupMessage(textBox, "Invalid value! Max 0x3F0!");
+                e.Handled = true;
+                return;
+            }
+
+            e.Handled = true;
+        }
+
+        private void HexDataTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            string input = e.Text.ToUpper();
+
+            if (!Regex.IsMatch(input, "[0-9A-F]"))
+            {
+                ShowPopupMessage(textBox, "Invalid character! Use 0-9, A-F");
+                e.Handled = true;
+                return;
+            }
+
+            // Predict the new text if input is accepted
+            string newText = textBox.Text.Insert(textBox.SelectionStart, input);
+
+            if (newText.Length > 4)
+            {
+                ShowPopupMessage(textBox, "Invalid length! Max 4 symbols!");
+                e.Handled = true;
+                return;
+            }
+
+
+            // Ensure new text does not exceed MAXVALUE
+            if (TryParseHex(newText, out uint parsedValue) && parsedValue <= 0xffff)
+            {
+                textBox.Text = newText;
+                textBox.SelectionStart = textBox.Text.Length;
+            }
+            else if (parsedValue > 0xffff)
+            {
+                ShowPopupMessage(textBox, "Invalid value! Max 0x3F0!");
+                e.Handled = true;
+                return;
+            }
+
+            e.Handled = true;
+        }
+
+        private void HexFullTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            string input = e.Text.ToUpper();
+
+            if (!Regex.IsMatch(input, "[0-9A-F]"))
+            {
+                ShowPopupMessage(textBox, "Invalid character! Use 0-9, A-F");
+                e.Handled = true;
+                return;
+            }
+
+            // Predict the new text if input is accepted
+            string newText = textBox.Text.Insert(textBox.SelectionStart, input);
+
+            if (newText.Length > 8)
+            {
+                ShowPopupMessage(textBox, "Invalid length! Max 8 symbols!");
+                e.Handled = true;
+                return;
+            }
+
+
+            // Ensure new text does not exceed MAXVALUE
+            if (TryParseHex(newText, out uint parsedValue) && parsedValue <= uint.MaxValue)
+            {
+                textBox.Text = newText;
+                textBox.SelectionStart = textBox.Text.Length;
+            }
+            else if (parsedValue > uint.MaxValue)
+            {
+                ShowPopupMessage(textBox, "Invalid value! Max UINT!");
+                e.Handled = true;
+                return;
+            }
+
+            e.Handled = true;
+        }
+
+        private bool TryParseHex(string hexString, out uint result)
+        {
+            return uint.TryParse(hexString, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out result);
         }
     }
 }
