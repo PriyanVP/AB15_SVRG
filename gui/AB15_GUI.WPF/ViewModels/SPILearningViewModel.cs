57476b7e1e78895e73e576935c5f70bb517f52b5
Sandalski Danail (RBBG/ENG21) <sad1sf@bosch.com>
2025-02-20 14:59:03 +0200

    ABEVBSW-201 Fixed 9 - Added tooltips to all UI elements

--- gui/AB15_GUI.WPF/ViewModels/SPILearningViewModel.cs
+++ gui/AB15_GUI.WPF/ViewModels/SPILearningViewModel.cs
@@ -176,14 +176,37 @@ namespace AB15_GUI.WPF.ViewModels
         /// </summary>
         private void InitHelpMessages()
         {
-            // TODO: add later
             // Bindable properties help messages
-            // AddHelpMsg(nameof(), $"");
+            AddHelpMsg(nameof(IsRawSPISelected), "Enable or disable the RAW value input");
+            AddHelpMsg(nameof(RefToActiveRecord.Time), "Frame Time");
+            // MOSI
+            AddHelpMsg(nameof(RefToActiveRecord.MOSI.Address), "Register address (10 Bit)");
+            AddHelpMsg(nameof(RefToActiveRecord.MOSI.RwFlag), "Indicates transfer direction for the address:\r\n R = read\r\n W = write");
+            AddHelpMsg("MOSI" + nameof(RefToActiveRecord.MOSI.Data), "Data being written into registers");
+            AddHelpMsg("MOSI" + nameof(RefToActiveRecord.MOSI.CRC), "Checksum input: Calculated using bits 31 to 5 of MOSI frame");
+            AddHelpMsg(nameof(RefToActiveRecord.MOSI.RawMOSI), "RAW Receive Frame (MOSI)");
+            // MISO
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.GS5), "Transfer Failure Flag (TFF):\r\n 0 = OK\r\n 1 = Failure in previous SPI Communication");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.GS4), "Test Active Flag (TST):\r\n 0 = OK\r\n 1 = ASIC in test mode");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.GS3), "End Of Programming (EOP):\r\n 0 = EOP1 and EOP2 set\r\n 1 = EOP1 and/or EOP2 not set");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.GS2), "APB Bus Transaction Error (APB):\r\n 0 = OK\r\n 1 = Failure in the previous Read/Write APB transaction");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.GS1), "ADC Error (ADC):\r\n 0 = OK\r\n 1 = ADC error has occurred");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.GS0), "APB Access Time Error (ATE):\r\n 0 = Ok\r\n 1 = APB bus access from the previous SPI frame was not executed on time and correctness previous request is not guaranteed");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.SF), "N/A");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.S0), "ASIC Error Flag:\r\n 0 = ASIC error not present\r\n 1 = Oscillator Monitor Error (Oscillator failure) or Current APB Read Access Time Error (ATE) or Current APB Read Bus Transaction Error (APB)");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.AdditionalStatus), "Additional Status flags");
+            AddHelpMsg("MISO" + nameof(RefToActiveRecord.MISO.Data), "Data read from the register (Set to 0 in case of SPI WR transaction)");
+            AddHelpMsg("MISO" + nameof(RefToActiveRecord.MISO.CRC), "Checksum output: Calculated using bits 31 to 3 of MISO frame");
+            AddHelpMsg(nameof(RefToActiveRecord.MISO.RawMISO), "RAW Transmit Frame (MISO)");
 
             // Commands
-            // AddHelpMsg(nameof(), $"");
+            AddHelpMsg(nameof(SPICommand), "Execute the selected SPI command");
 
             // UI elements help messages
+            AddHelpMsg("RW", "Indicates transfer direction for the address:\r\n 0 = read\r\n 1 = write");
+            AddHelpMsg("NA", "Unused constant for bit 0 and bit 1");
+            AddHelpMsg("ReadWriteSwitch", "Switch between Read and Write frame type");
+            AddHelpMsg("DeviceIDSelector", "Device ID");
         }
 
         #endregion // Bindable_Properties
