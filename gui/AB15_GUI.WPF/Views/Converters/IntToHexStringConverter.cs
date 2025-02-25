d7ec9934cf426ba4a01b9e341af550d0b5841567
Oleksii Dudnyk <oleksii.dudnyk@ua.bosch.com>
2025-02-25 10:29:32 +0200

    GUI: implemented adjastable two way int to hex string converter

--- gui/AB15_GUI.WPF/Views/Converters/IntToHexStringConverter.cs
+++ gui/AB15_GUI.WPF/Views/Converters/IntToHexStringConverter.cs
@@ -14,33 +14,56 @@ namespace AB15_GUI.WPF.Views.Converters
         /// </summary>
         /// <param name="value">unsigned value of input</param>
         /// <param name="parameter">length of output string</param>
-        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             try
             {
                 // Typecast to actual types. Incorrect typecast is handled by try-catch
-                uint inputNumber = (uint) value;
-                int bitPosition = System.Convert.ToInt32(parameter);
+                uint inputNumber = (uint)value;
+                int outputLength = (parameter != null) ? System.Convert.ToInt32(parameter) : 4; // defaults to 4
 
                 // Validate bit position range
-                if (bitPosition < 0 || bitPosition > 31)
+                if (outputLength < 1 || outputLength > 16)
                     return "0";
 
-                return (inputNumber & (1 << bitPosition)) != 0 ? "1" : "0";
+                return $"0x{inputNumber.ToString("X").PadLeft(outputLength, '0')}";
             }
             catch
             {
-                return "0";
+                return "Err";
             }
         }
-
+       
         /// <summary>
-        /// Unused method
+        /// Convert hex string to int number
         /// </summary>
+        /// <param name="value">string input as hex (0xXX or XX)</param>
         public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Typecast to actual types. Incorrect typecast is handled by try-catch
+                string hexString = value.ToString();
+                if (string.IsNullOrEmpty(hexString))
+                {
+                    return 0;
+                }
+
+                // Remove "0x" prefix if present
+                if (hexString.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
+                {
+                    hexString = hexString.Substring(2);
+                }
+
+                // Convert hex string to integer
+                uint inputNumber = uint.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+
+                return inputNumber;
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
