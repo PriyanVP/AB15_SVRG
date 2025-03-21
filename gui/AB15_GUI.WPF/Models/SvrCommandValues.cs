add8b82e4c7afbb73ec7411712053889e4f4a173
Sven Markwart (ME/PJ-SW1) <Sven.Markwart@de.bosch.com>
2025-03-21 09:24:01 +0100

    Reordered SVR command value definitions to be ascending

--- gui/AB15_GUI.WPF/Models/SvrCommandValues.cs
+++ gui/AB15_GUI.WPF/Models/SvrCommandValues.cs
@@ -5,9 +5,9 @@
     /// </summary>
     public enum SvrCommandValues
     {
-        SVR_NO_CHANGE = 0xFF,
         SVR_OFF = 0x00,
         SVR_ON = 0x11,
+        SVR_NO_CHANGE = 0xFF,
     }
 
 }
